6948fff3438ecb2ebb4ed7ae1cd9a6d11de96ae1
AsifAhmedTanjid <111447098+AsifAhmedTanjid@users.noreply.github.com>
2023-04-06 05:07:01 +0600

    database (search and delete added)

---
+++ project v6/views/vendorlist.php
@@ -0,0 +1,186 @@
+<?php
+require_once '../models/usermodel.php';
+session_start();
+if(isset($_COOKIE['flag'])){
+?>
+
+
+
+<html>
+    <head>
+        <title>Admin Dashboard</title>
+    </head>
+    <body>
+    <center>
+        <table  height=720 width=1080>
+            <tr height=70>
+
+                <td colspan="2"> 
+                    <table width = "800">
+                    <tr>
+                        <td>
+                            <img height="120" src="../assets/logo.png">
+                        </td>
+                        <td align="right">
+                        Logged in as <a href="viewprofile.php"> <?php echo $_SESSION['name'] ?></a> |
+                                    <a href="../controllers/logout.php">Logout</a>
+                    </td>    
+                </tr>    
+                </table>
+                </td>
+            </tr>
+            <tr>
+
+
+
+
+
+
+
+
+
+
+
+
+                <td width="300">
+                    
+                <ul>
+                    <!-- <li> <a href="admindashboard.php">Admin Dashboard</a></li>
+                    <li> <a href="search.php">Search</a></li>
+                        <li> <a href="viewprofile.php">  Profile</a></li>
+                        <li> <a href="setting.php">Settings</a></li>
+                        
+                        <li> <a href="logout.php"> Logout</a></li> -->
+
+                     <?php
+                        if (trim($_SESSION['role'])=="admin")
+                    {
+                        ?>
+                        <li> <a href="admindashboard.php">Admin Dashboard</a></li>
+                        <li> <a href="viewprofile.php">  Profile</a></li>
+                        <li> <a href="setting.php">Settings</a></li>
+                        
+                        <li> <a href="../controllers/logout.php"> Logout</a></li> 
+                    <?php
+                    }
+                    else if (trim($_SESSION['role'])=="vendor")
+                    {
+                        ?>
+
+                        <li> <a href="viewprofile.php">  Profile</a></li>
+                        <li> <a href="setting.php">Settings</a></li>
+                        
+                        <li> <a href="../controllers/logout.php"> Logout</a></li>
+
+                        <?php
+                    }
+                    else if (trim($_SESSION['role'])=="customer")
+                    {
+                        ?>
+                         <li> <a href="search.php">Search</a></li>
+                        <li> <a href="viewprofile.php">  Profile</a></li>
+                        <li> <a href="setting.php">Settings</a></li>
+                        
+                        <li> <a href="../controllers/logout.php"> Logout</a></li>
+                        <?php
+                    }
+                    else if (trim($_SESSION['role'])=="deliveryman")
+                    {
+                        ?>
+                        <li> <a href="viewprofile.php">  Profile</a></li>
+                        <li> <a href="setting.php">Settings</a></li>
+                        
+                        <li> <a href="../controllers/logout.php"> Logout</a></li>
+                        <?php
+                    }
+
+                    ?>
+                    </ul>
+
+                </td>
+
+
+
+
+                <td>
+
+                        <fieldset>
+                                <legend> Vendor list </legend>
+                                <table border="1">
+                                    <tr> 
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Username</th>
+                                        <th>Email</th>
+                                        <th>Gender</th>
+                                        <th>Date Of Birth</th>
+                                        <th>Address</th>
+                                        <th>Joining Date</th>
+                                        <th>Phone Number</th>
+                                        <th>Action</th>
+
+                                    </tr>
+<?php
+$role="vendor";
+viewuser($role);
+
+?>
+
+                                    <!-- <tr>
+                                        <td>A01</td>
+                                        <td>Tanjid</td>
+                                        <td>tanjid01</td>
+                                        <td>tanjid@gmail.com</td>
+                                        <td>Male</td>
+                                        <td>1998-01-03</td>
+                                        <td>Sonirakhra</td>
+                                        <td><input type="button" name="delete" value="Delete"/></td>
+
+                                    </tr> -->
+                                
+
+                                </table>
+                        </fieldset>
+
+
+                </td>
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </tr>
+
+
+            <tr>
+                <td colspan="2" align="center"><fieldset>
+                    <legend>Help & Support</legend>
+                    <a href="">Contact Us</a>  <br>
+                    <a href="">Complain</a> <br>
+                    <a href="">FAQ</a> <br>
+            </fieldset></td>
+            </tr>
+
+
+
+            <tr height ="40" >
+                <td colspan="2" align="center">Copyright © 2023</td>
+            </tr>
+        </table>
+    </center>
+    </body>
+</html>
+
+<?php
+}else{
+    header('location: login.php'); 
+}
+?>
